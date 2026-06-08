801f1eb791ba8f243541258c9aa12b7073d77459
yousuf khann <mykhann10@gmail.com>
2026-06-08 23:18:32 +0500

    feat (server) : add express.json for parsing incoming http requests

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express"
 import leadRoutes from "./src/routes/lead.routes.js"
 import { connectDB } from "../../ai-support-agent/backend/src/config/db.js"
+import "dotenv/config"
 const app= express()
 
 
 const PORT= process.env.PORT || 3001
 
+app.use(express.json())
 // database connection 
 connectDB()
 // routes 
