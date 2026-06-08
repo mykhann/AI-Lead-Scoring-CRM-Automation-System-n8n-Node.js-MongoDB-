7c03a8a475a16704ed233f576b593acdab218a86
yousuf khann <mykhann10@gmail.com>
2026-06-08 23:17:16 +0500

    feat (server) : call db func

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express"
 import leadRoutes from "./src/routes/lead.routes.js"
+import { connectDB } from "../../ai-support-agent/backend/src/config/db.js"
 const app= express()
 
 
 const PORT= process.env.PORT || 3001
 
-
+// database connection 
+connectDB()
 // routes 
 app.use("/api/leads",leadRoutes)
 
